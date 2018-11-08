1cc32bf0b408f8dc5985b46006c57b09fc2b6334
wangqi <873790387@qq.com>
2018-11-08 19:04:25 +0800

    'nine'

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/getinitdata', getinitdataRouter);
 
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, './dist/index.html'))
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
